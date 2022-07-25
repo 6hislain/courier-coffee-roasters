25fb1c4c5d49ee1be3159cd09f7bd80992eca9b2
6hislain <ghislainmike80@gmail.com>
2022-07-25 14:40:25 +0200

    animations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+NProgress.start();
+AOS.init();
 const topButton = document.getElementById("back-to-top");
+
 if (topButton)
   window.onscroll = function () {
     if (
@@ -15,11 +18,16 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
-    nav: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    animateOut: "slideOutDown",
+    animateIn: "flipInX",
     responsive: {
-      0: {  items: 1, },
-      600: { items: 2,  },
-      1000: {  items: 3, },
+      0: { items: 1 },
+      600: { items: 2 },
+      1000: { items: 3 },
     },
   });
+  NProgress.done();
 });
